0b5abc7035340dae60bfaca04812abb67e1969de
Vladimir Khokhlov <vladimirkhokhlov90@gmail.com>
2020-12-09 23:30:05 +0400

    add method "guessTheNumber"

--- src/ru/geekbrains/lesson3/Lesson3.java
+++ src/ru/geekbrains/lesson3/Lesson3.java
@@ -1,7 +1,33 @@
 package ru.geekbrains.lesson3;
 
-public class Lesson3 {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Lesson3 {
+    public static void main(String[] args){
+        guessTheNumber();
+        }
+
+    public static void guessTheNumber(){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int a;
+        int b;
+        b = random.nextInt(10);
+        System.out.println("Введите число от 0 до 9");
+        do {
+            a = scanner.nextInt();
+
+            if (a < 0 || a > 9) {
+            System.out.println("Дурень, введи от 0 до 9");
+            }
+            else if (a < b){
+            System.out.println("загаданное чилсло больеше");
+            }
+            else if (a > b){
+            System.out.println("загаданное число меньше");
+            }
+        }
+        while (a != b);
     }
 }
