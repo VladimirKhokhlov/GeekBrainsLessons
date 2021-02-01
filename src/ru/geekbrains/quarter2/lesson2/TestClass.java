e7fc90d26d99d4999a66b4a410dd7299b6f03a23
Vladimir Khokhlov <vladimirkhokhlov90@gmail.com>
2021-02-01 20:49:29 +0400

    Start homework to lesson 2

---
+++ src/ru/geekbrains/quarter2/lesson2/TestClass.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.quarter2.lesson2;
+
+public class TestClass {
+    public static void main(String[] args) {
+        stringArray();
+    }
+
+    public static void stringArray() {
+//        String[][] stringArray = {{"1","w","2","3"}, {"8","6","q","i"},{"3","6","0","9"},{"1","3","y","6"}};
+        String[][] stringArray = new String[4][4];
+        for (String[] row : stringArray) {
+            for (String col : row) {
+
+            }
+        }
+    }
+}
